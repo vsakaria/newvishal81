4801b221a2298c73822c6b661c630fcdb41a6e33
Vishal Sakaria <vsakaria@gmail.com>
2013-04-29 13:06:54 +0100

    First new commit

---
+++ js/animation.js
@@ -0,0 +1,21 @@
+
+
+$("div#work").click( function(){
+
+  console.log("work triggered")
+
+  $("div#home_main_text_container").css("display", "none");
+  $("div#work_main_text_container").css("display", "block");
+
+});
+
+
+$("div#home").click( function(e){
+
+  console.log("home triggered")
+
+  $("div#work_main_text_container").css("display", "none");
+  $("div#home_main_text_container").css("display", "block");
+
+
+});
