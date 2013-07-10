e344834d3349b1700b3676ec02c7ca38c44a63df
Vishal Sakaria <vsakaria@gmail.com>
2013-07-10 15:34:40 +0100

    New portfoilo link created

--- js/animation.js
+++ js/animation.js
@@ -1,39 +1,18 @@
 
 
 $("div#work").click( function(){
-
-  console.log("work triggered")
-
-  $("div#home_main_text_container").css("display", "none");
-  $("div#work_main_text_container").css("display", "block");
-
+	$("div#home_main_text_container").fadeOut('slow').css("display", "none");
+	$("div#work_main_text_container").fadeIn('slow').css("display", "block");
 });
 
 
 $("div#home").click( function(e){
-
-  console.log("home triggered")
-
-  $("div#work_main_text_container").css("display", "none");
-  $("div#home_main_text_container").css("display", "block");
-
-
+	$("div#work_main_text_container").fadeOut('slow').css("display", "none");
+	$("div#home_main_text_container").fadeIn('slow').css("display", "block");
 });
 
 $("#video").on('hide', function(e) {
-
-	console.log("back droptrigger")
 	$('iframe#pres_video').attr('src','');
 	$('iframe#pres_video').attr('src','http://player.vimeo.com/video/65318374');
 });
 
-// $(function() {
-
-//   console.log("doc ready")
-
-// $('div#video_link').effect("bounce", { times:10 }, 300);
-
-// });
-// $("a#video").click( function(e){
-//     $('#video').modal()
-// });
