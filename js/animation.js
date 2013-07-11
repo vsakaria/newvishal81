e9dbc1f4118f522dc032a00b0066cc4ab55fb030
Vishal Sakaria <vsakaria@gmail.com>
2013-07-11 14:57:15 +0100

    Icons, new git and portfoilo projects

--- js/animation.js
+++ js/animation.js
@@ -1,12 +1,14 @@
 
 
 $("div#work").click( function(){
+	console.log("Work pressed")
 	$("div#home_main_text_container").fadeOut('slow').css("display", "none");
 	$("div#work_main_text_container").fadeIn('slow').css("display", "block");
 });
 
 
 $("div#home").click( function(e){
+	console.log("Home pressed")
 	$("div#work_main_text_container").fadeOut('slow').css("display", "none");
 	$("div#home_main_text_container").fadeIn('slow').css("display", "block");
 });
@@ -15,4 +17,8 @@ $("#video").on('hide', function(e) {
 	$('iframe#pres_video').attr('src','');
 	$('iframe#pres_video').attr('src','http://player.vimeo.com/video/65318374');
 });
+
+
+
+
 
